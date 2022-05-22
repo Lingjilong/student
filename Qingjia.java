d5eee469f34f3fb7c33075e8a82a309e83251dfd
LingJiLong <1319335449@qq.com>
2022-05-22 23:18:28 +0800

    update7

--- Qingjia.java
+++ Qingjia.java
@@ -1,1 +1,6 @@
-public class Qingjia{}
+public class Qingjia{
+    public static void main(String[] args) {
+        System.out.println("=========请一天假============");
+        System.out.println("=========请两天假============");
+    }
+}
