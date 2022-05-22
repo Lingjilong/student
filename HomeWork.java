f711bad33c8f54ddd06facc99c4015deed8eb4ee
LingJiLong <1319335449@qq.com>
2022-05-22 21:48:16 +0800

    update2

--- HomeWork.java
+++ HomeWork.java
@@ -1,11 +1,11 @@
 public class HomeWork{
 
-	//锟节家匡拷锟斤拷锟斤拷锟斤拷锟斤拷
+	//在家开发的内容
 	private String content;
 	private String name;
 	private String time;
 
-	//锟节癸拷司锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+	//在公司开发的内容
 	private int id;
 
 	//在上海开发的内容
