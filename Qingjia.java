1f161a8314983462672a5a5233072916521eb109
LingJiLong <1319335449@qq.com>
2022-05-22 23:31:30 +0800

    dayOff

--- Qingjia.java
+++ Qingjia.java
@@ -2,5 +2,7 @@ public class Qingjia{
     public static void main(String[] args) {
         System.out.println("=========请一天假============");
         System.out.println("=========请两天假============");
+        System.out.println("=========再请两天假============");
+        System.out.println("=========继续请两天假============");
     }
 }
