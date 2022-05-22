09a51c61f913f9c34d74fd3e2f53486078763efb
LingJiLong <1319335449@qq.com>
2022-05-22 23:29:55 +0800

    writeQuestionAndAnswer

--- AnswerSheet.java
+++ AnswerSheet.java
@@ -3,4 +3,8 @@ public class AnswerSheet{
     String answer2;
     String answer3;
     String answer4;
+    String question1;
+    String question2;
+    String question3;
+    String question4;
 }
