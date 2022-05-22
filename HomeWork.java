9bb9355e6b635196bc03f60fd6756a8bc4575a1c
LingJiLong <1319335449@qq.com>
2022-05-22 21:48:16 +0800

    new update
    update2
    
    update3
    
    update4
    
    update 5
    
    update6
    
    update7
    
    dayOff
    
    dayOff2
    
    day5555
    
    day5555

--- HomeWork.java
+++ HomeWork.java
@@ -1,8 +1,67 @@
 public class HomeWork{
-
+    //预备工作
+	private String prepare;
 	//在家开发的内容
-	private String content
+	private String content;
+	private String name;
+	private String time;
 
 	//在公司开发的内容
 	private int id;
+
+	//在上海开发的内容
+	private int numbers;
+
+
+	//just for test
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumbers() {
+		return numbers;
+	}
+
+	public void setNumbers(int numbers) {
+		this.numbers = numbers;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
