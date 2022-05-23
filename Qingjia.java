0cae0ebc83bc7abc43b18291db81196636572b75
LingJiLong <1319335449@qq.com>
2022-05-23 16:30:43 +0800

    day66

--- Qingjia.java
+++ Qingjia.java
@@ -1,9 +1,14 @@
-public class Qingjia{
+public class Qingjia {
     public static void main(String[] args) {
         System.out.println("=========请一天假============");
         System.out.println("=========请两天假============");
         System.out.println("=========再请两天假============");
         System.out.println("=========继续请两天假============");
         System.out.println("=========请，接着请============");
+        System.out.println("=========请，接着请11============");
+        System.out.println("=========请，接着请22============");
+        System.out.println("=========请，接着请33============");
+        System.out.println("=========请，接着请44============");
+        System.out.println("=========请，接着请55============");
     }
 }
